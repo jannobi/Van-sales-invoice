cdfa15f86234e160ac958b73a08d0bd0edc8cb6a
jannobi <seconddoremon@gmail.com>
2026-08-30 19:58:37 +0600

    Create salesmanController.js

---
+++ backend/src/controllers/salesmanController.js
@@ -0,0 +1,5 @@
+const Salesman = require('../models/Salesman');
+const Item = require('../models/Item');
+const Inventory = require('../models/Inventory');
+const Party = require('../models/Party');
+const Invoice = require('../models/Invoice
